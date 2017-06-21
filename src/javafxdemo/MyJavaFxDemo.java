365ae254fde81463ae6d1de02a0257b4dbac709a
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-06-21 21:58:06 +0000

    Premiere Fenétre JavaFX

---
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxdemo;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+/**
+ *
+ * @author super
+ */
+public class MyJavaFxDemo extends Application {
+    Button button;
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the window");
+        button=new Button();
+        button.setText("Click Me");
+        
+         
+        StackPane layout=new StackPane();
+        layout.getChildren().add(button);
+        
+        Scene scene= new Scene(layout,300,250);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        
+    }
+
+   
+    }
+    
+    
+    
+    
+    
+}
