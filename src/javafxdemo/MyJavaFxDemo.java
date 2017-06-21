a53c8626c68331cba681f3226b4c881118a061f2
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-06-21 23:36:32 +0000

    Alerte Window

--- src/javafxdemo/MyJavaFxDemo.java
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,47 +10,38 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 /**
  *
  * @author super
  */
 public class MyJavaFxDemo extends Application {
-    Stage window;
-    Scene scene1,scene2;
+    Button button;
     public static void main(String[] args){
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window=primaryStage;
+        primaryStage.setTitle("Title of the window");
+        button=new Button();
+        button.setText("Click Me");
+        button.setOnAction(e->AlertBox.display("Alert", "What do you think"));
         
-        Label label1=new Label("Welcome to the first scene");
-        Button button1=new Button("Go to scene 2");
-        button1.setOnAction(e->window.setScene(scene2));
+         
+        StackPane layout=new StackPane();
+        layout.getChildren().add(button);
         
-        //Layout 1 - children are laid out in vertical culumn
-        VBox layout1=new VBox(20);
-        layout1.getChildren().addAll(label1,button1);
-        scene1= new Scene(layout1,200,200);
+        Scene scene= new Scene(layout,300,250);
         
-        //Button 2
-        Button button2=new Button("This scene sucks go to scene 2");
-        button2.setOnAction(e->window.setScene(scene1));
+        primaryStage.setScene(scene);
+        primaryStage.show();
         
-        //Layout2
-        StackPane layout2=new StackPane();
-        layout2.getChildren().add(button2);
-        scene2=new Scene(layout2,600,300);
         
-        window.setScene(scene1);
-        window.setTitle("Title here");
-        window.show();
     }
 
-   
+    
+    
+    
 }
