276bddca3212d6e1c8d11bea1639092d5666f2bb
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-06-21 22:52:23 +0000

    Switchin Scenes

--- src/javafxdemo/MyJavaFxDemo.java
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -9,34 +9,46 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 /**
  *
  * @author super
  */
 public class MyJavaFxDemo extends Application {
-    Button button;
+    Stage window;
+    Scene scene1,scene2;
     public static void main(String[] args){
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the window");
-        button=new Button();
-        button.setText("Click Me");
-        button.setOnAction(e->System.out.println("Using lambda Expression"));
-         
-        StackPane layout=new StackPane();
-        layout.getChildren().add(button);
+        window=primaryStage;
         
-        Scene scene= new Scene(layout,300,250);
+        Label label1=new Label("Welcome to the first scene");
+        Button button1=new Button("Go to scene 2");
+        button1.setOnAction(e->window.setScene(scene2));
         
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Layout 1 - children are laid out in vertical culumn
+        VBox layout1=new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
+        scene1= new Scene(layout1,200,200);
         
+        //Button 2
+        Button button2=new Button("This scene sucks go to scene 2");
+        button2.setOnAction(e->window.setScene(scene1));
         
+        //Layout2
+        StackPane layout2=new StackPane();
+        layout2.getChildren().add(button2);
+        scene2=new Scene(layout2,600,300);
+        
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
     }
 
    
