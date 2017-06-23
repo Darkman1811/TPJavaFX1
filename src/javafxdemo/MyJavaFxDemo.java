eebab26bd67309356bbbc705fadec21098879e56
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-06-23 02:55:55 +0000

    Checkboxs

--- src/javafxdemo/MyJavaFxDemo.java
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -35,16 +36,18 @@ public class MyJavaFxDemo extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window=primaryStage;
-        window.setTitle("Form Data");
+        window.setTitle("Checkbox example");
         
         //frm
-        TextField nameImput= new TextField();
-        button=new Button("Click Me");
-        button.setOnAction(e->isInt(nameImput));
+       CheckBox box1=new CheckBox("Bacon");
+       CheckBox box2=new CheckBox("Tuna");
+       box2.setSelected(true);
+        button=new Button("Order now");
+        button.setOnAction(e->handleOptions(box1,box2));
         
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(nameImput,button);
+        layout.getChildren().addAll(box1,box2,button);
         
         scene = new Scene(layout,300,200);
         window.setScene(scene);      
@@ -52,18 +55,22 @@ public class MyJavaFxDemo extends Application {
         
     }
 
-    private boolean isInt(TextField input) {
-        try{
-            int age = Integer.parseInt(input.getText());
-            System.out.println("User is:"+age);
-            return true;
-        }
-        catch(NumberFormatException e){
-            System.out.println("Error: "+input.getText()+" is not a number");            
-             return false;
-        }
-       
+    private void handleOptions(CheckBox box1, CheckBox box2) {
+      String message="Users order: \n";
+      
+      if(box1.isSelected()){
+          message+="- Bacon\n";
+      }
+      
+      if(box2.isSelected()){
+          message+="- Tuna\n";
+      }
+      
+      System.out.println(message);
+          
     }
+
+    
 
    
 }
