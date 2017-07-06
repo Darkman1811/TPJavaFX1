36ec13fe023832341fc7b902af8dcd929708460b
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-07-06 02:10:12 +0000

    TableView

--- src/javafxdemo/MyJavaFxDemo.java
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -7,6 +7,7 @@
 package javafxdemo;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,9 +19,12 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -37,7 +41,7 @@ public class MyJavaFxDemo extends Application {
     Stage window;
     Scene scene;
     Button button;
-    TreeView<String> tree;
+    TableView<Product> table;
 
     public static void main(String[] args) {
         launch(args);
@@ -48,62 +52,46 @@ public class MyJavaFxDemo extends Application {
         window = primaryStage;
         window.setTitle("Checkbox example");
 
-        TreeItem<String> root, bucky, megan;
-
-        root = new TreeItem<>();
-        root.setExpanded(true);
-
-      /*  bucky = new TreeItem<>("bucky");
-        TreeItem dodo = new TreeItem<>("dodo");
-        TreeItem dada = new TreeItem<>("dada");
-        TreeItem didi = new TreeItem<>("didi");
-
-        bucky.getChildren().addAll(dodo, dada, didi);
-
-        megan = new TreeItem<>("megan");
-        TreeItem mago = new TreeItem<>("mago");
-        TreeItem bagass = new TreeItem<>("bagass");
-        megan.getChildren().addAll(mago, bagass);
-
-        root.getChildren().add(bucky);
-
-        bucky.getChildren().add(megan);
-              */
-        bucky=makeBranch("Bucky", root);
-        makeBranch("dodo", bucky);
-        makeBranch("dada", bucky);
-        makeBranch("didi", bucky);
+        //Name Column
+        TableColumn<Product,String> nameColumn=new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         
-        megan=makeBranch("megan", root);
-        makeBranch("mago", megan);
-        makeBranch("bagass", megan);
+        //Price Column
+        TableColumn<Product,Double> priceColumn=new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));       
         
-        tree = new TreeView<>(root);
-        tree.setShowRoot(false);
-        
-        tree.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue)->{
-            if(newValue!=null){
-                System.out.println(newValue.getValue());
-            }
-        });
+          //Quantity Column
+        TableColumn<Product,Integer> quantityColumn=new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(100);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity")); 
 
+        table=new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn,priceColumn,quantityColumn);
+        
         button = new Button("Order now");
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(tree, button);
+        layout.getChildren().addAll(table, button);
 
-        scene = new Scene(layout, 300, 200);
+        scene = new Scene(layout, 600, 400);
         window.setScene(scene);
         window.show();
 
     }
 
-    public TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
-        TreeItem<String> item = new TreeItem<>(title);
-        item.setExpanded(true);
-        parent.getChildren().add(item);
-        return item;
-    }
+    //Get all of the product
+  public ObservableList<Product> getProduct(){
+      ObservableList<Product> products=FXCollections.observableArrayList();
+      products.add(new Product("Laptop",859,20));
+      products.add(new Product("Bouncy Ball",2.49,198));
+      products.add(new Product("Toilet",99,74));
+      products.add(new Product("The NoteBook DVD",19.99,12));
+      products.add(new Product("Corn",1.49,856));
+      return products;
+  }
 
 }
