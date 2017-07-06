88dc00dda52d9e88d283ff413acbb32cf3676daa
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-07-06 01:37:03 +0000

    TreeView

--- src/javafxdemo/MyJavaFxDemo.java
+++ src/javafxdemo/MyJavaFxDemo.java
@@ -5,6 +5,7 @@
  * and open the template in the editor.
  */
 package javafxdemo;
+
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,69 +19,91 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 /**
  *
  * @author super
  */
 public class MyJavaFxDemo extends Application {
+
     Stage window;
     Scene scene;
     Button button;
-    ListView<String> listView;
-    
-    public static void main(String[] args){
+    TreeView<String> tree;
+
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window=primaryStage;
+        window = primaryStage;
         window.setTitle("Checkbox example");
+
+        TreeItem<String> root, bucky, megan;
+
+        root = new TreeItem<>();
+        root.setExpanded(true);
+
+      /*  bucky = new TreeItem<>("bucky");
+        TreeItem dodo = new TreeItem<>("dodo");
+        TreeItem dada = new TreeItem<>("dada");
+        TreeItem didi = new TreeItem<>("didi");
+
+        bucky.getChildren().addAll(dodo, dada, didi);
+
+        megan = new TreeItem<>("megan");
+        TreeItem mago = new TreeItem<>("mago");
+        TreeItem bagass = new TreeItem<>("bagass");
+        megan.getChildren().addAll(mago, bagass);
+
+        root.getChildren().add(bucky);
+
+        bucky.getChildren().add(megan);
+              */
+        bucky=makeBranch("Bucky", root);
+        makeBranch("dodo", bucky);
+        makeBranch("dada", bucky);
+        makeBranch("didi", bucky);
         
-        listView= new ListView<>();
-        //getItmes returns the observablelist object whick you can add items to
-        listView.getItems().add("Apples");
-        listView.getItems().add("Banana");
-        listView.getItems().addAll("Bacon","Ham","MeatBalls");
-        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        megan=makeBranch("megan", root);
+        makeBranch("mago", megan);
+        makeBranch("bagass", megan);
         
-        button=new Button("Order now");
-        button.setOnAction(e->buttonClicked());
+        tree = new TreeView<>(root);
+        tree.setShowRoot(false);
         
+        tree.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue)->{
+            if(newValue!=null){
+                System.out.println(newValue.getValue());
+            }
+        });
+
+        button = new Button("Order now");
+
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(listView,button);
-        
-        scene = new Scene(layout,300,200);
-        window.setScene(scene);      
-        window.show();        
-        
+        layout.getChildren().addAll(tree, button);
+
+        scene = new Scene(layout, 300, 200);
+        window.setScene(scene);
+        window.show();
+
     }
 
-   
-
-    private void buttonClicked() {
-     String message = "";
-        ObservableList<String> movies=listView.getSelectionModel().getSelectedItems();
-        for(String m: movies){
-            message+=m + "\n";
-        }
-        
-        System.out.println(message);
-        
+    public TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
+        TreeItem<String> item = new TreeItem<>(title);
+        item.setExpanded(true);
+        parent.getChildren().add(item);
+        return item;
     }
 
-  
-
-  
-
-    
-
-   
 }
